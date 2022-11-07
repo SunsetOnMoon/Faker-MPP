5d8c5bc296e5a2794dc3583914a9ee0fe9a4957d
Artsem Kulik <artsem.kulik@gmail.com>
2022-11-07 19:41:31 +0300

    Added Generators for simple data types

---
+++ Faker/Faker.Core/SimpleTypesGenerator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faker.Faker.Core
+{
+    public class IntGenerator : Generator<int>
+    {
+        public override int ObjectGeneration(Random random)
+        {
+            int randomValue = random.Next();
+            return randomValue;
+        }
+    }
+
+    public class FloatGenerator : Generator<float>
+    {
+        public override float ObjectGeneration(Random random)
+        {
+            float randomValue = (float)(random.NextDouble()) * random.Next();
+            return randomValue;
+        }
+    }
+
+    public class LongGenerator : Generator<long>
+    {
+        public override long ObjectGeneration(Random random)
+        {
+            long randomValue = random.Next()<<32 + random.Next();
+            return randomValue;
+        }
+    }
+
+    public class DoubleGenerator : Generator<double>
+    {
+        public override double ObjectGeneration(Random random)
+        {
+            double randomValue = (double)(random.NextDouble()) * random.Next();
+            return randomValue;
+        }
+    }
+
+    public class CharGenerator : Generator<char>
+    {
+        public override char ObjectGeneration(Random random)
+        {
+            char randomValue = (char)(random.Next(0, 255));
+            return randomValue;
+        }
+    }
+
+    public class BoolGenerator : Generator<bool>
+    {
+        public override bool ObjectGeneration(Random random)
+        {
+            bool randomValue = Convert.ToBoolean(random.Next(0, 1));
+            return randomValue;
+        }
+    }
+}
