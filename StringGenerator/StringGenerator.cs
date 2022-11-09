01c5b259c54c81e6d2cf4beb98b6a6ea515541e5
Artsem Kulik <artsem.kulik@gmail.com>
2022-11-09 19:57:46 +0300

    Added String Generator

---
+++ StringGenerator/StringGenerator.cs
@@ -0,0 +1,16 @@
+﻿using Faker.Core;
+namespace StringGenerator
+{
+    public class StringGenerator : Generator<string>
+    {
+        public override string ObjectGeneration(Random random)
+        {
+            byte[] byteArray = new byte[random.Next(10, 20)];
+
+            for (int i = 0; i < byteArray.Length; i++)
+                byteArray[i] = (byte)random.Next(90, 255);
+            string result = System.Text.Encoding.UTF8.GetString(byteArray);
+            return result;
+        }
+    }
+}
